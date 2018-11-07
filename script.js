96081b38ec2f3d43ceca5934c816f9866ce7fbd2
Julie Freund-Poulsen <juliefreund-poulsen@MacBook-Pro-5.local>
2018-11-07 16:07:34 +0100

    noget af gameover, settings og lyd

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ function sidenVises() {
     //Hvad der skal ske
     document.querySelector("#settings").addEventListener("click", showSettings);
     showStart();
+    //    document.querySelector("#start").addEventListener("click", toggleMusic);
 }
 
 function showStart() {
@@ -46,95 +47,52 @@ function toggleSounds() {
     if (showSettingsEffektSound == false) {
         console.log("true");
         showSettingsEffektSound = true;
+
         soundsOn();
     } else {
         console.log("false");
         showSettingsEffektSound = false;
+
         soundsOff();
         //        this.addEventListener("click", soundsOff);
     }
 }
 
+
 function soundsOff() {
     console.log("soundsOff");
-    document.querySelector("#sfx").classList.remove("on");
+    document.querySelector("#sfx").classList.add("off");
 
 }
 
 function soundsOn() {
     console.log("soundsOn");
-    document.querySelector("#sfx").classList.add("off");
+    document.querySelector("#sfx").classList.add("on");
 }
 
 function toggleMusic() {
     console.log("toggleMusic");
-    if (showSettingsMusic == true) {
+    if (showSettingsMusic == false) {
         console.log("true");
-        showSettingsMusic = false;
+        showSettingsMusic = true;
         musicOn();
     } else {
         console.log("false");
-        showSettingsMusic = true;
+        showSettingsMusic = false;
         musicOff();
     }
 }
 
 function musicOff() {
     console.log("musicOff");
-    document.querySelector("#music").classList.remove("on");
+    document.querySelector("#music").classList.add("off");
 }
 
 function musicOn() {
     console.log("musicOn");
-    document.querySelector("#music").classList.add("off");
+    document.querySelector("#music").classList.add("on");
 }
 
-//function hideCheckmark1() {
-//    console.log("hide checkmark1");
-//    document.querySelector("#checkmark1").classList.add("hide");
-//    //hide ved click
-//
-//
-//}
-//
-//function showCheckmark1() {
-//    console.log("show checkmark1");
-//
-//}
-//
-//
-//function hideCheckmark2() {
-//    console.log("hide checkmark2");
-//    document.querySelector("#checkmark2").classList.add("hide");
-//
-//    //hide ved click
-//
-//}
-//
-//function showCheckmark2() {
-//    console.log("show checkmark2");
-//
-//}
-//
-//function hideCheckmark3() {
-//    console.log("hide checkmark3");
-//    document.querySelector("#checkmark3").classList.add("hide");
-//
-//    //hide ved click
-//
-//}
-//
-//function showCheckmark3() {
-//    console.log("show checkmark3");
-//
-//}
-
-//
-//function showCheckmark1() {
-//    console.log("show checkmark1");
-//    document.querySelector("#checkmark1").classList.remove("hide");
-// document.querySelector("#play").classList.toggle("hide");
-//}
 
 function hideStart() {
     console.log("hide start");
@@ -150,53 +108,83 @@ function startGame() {
     document.querySelector("#start").removeEventListener("animationend", startGame);
     document.querySelector("#start").classList.add("hide");
 
-    document.querySelector("#meatball").addEventListener("click", clickFood);
-    document.querySelector("#meatball2").addEventListener("click", clickFood);
-    document.querySelector("#meatball3").addEventListener("click", clickFood);
-    document.querySelector("#meatball4").addEventListener("click", clickFood);
-    document.querySelector("#meatball5").addEventListener("click", clickFood);
-    document.querySelector("#meatball6").addEventListener("click", clickFood);
-    document.querySelector("#pasta").addEventListener("click", clickFood);
-    document.querySelector("#pasta2").addEventListener("click", clickFood);
-    document.querySelector("#pasta3").addEventListener("click", clickFood);
-    document.querySelector("#pasta4").addEventListener("click", clickFood);
-    document.querySelector("#pasta5").addEventListener("click", clickFood);
-    document.querySelector("#knife").addEventListener("click", clickCutlery);
-    document.querySelector("#knife2").addEventListener("click", clickCutlery);
-    document.querySelector("#knife3").addEventListener("click", clickCutlery);
-    document.querySelector("#knife4").addEventListener("click", clickCutlery);
-    document.querySelector("#fork").addEventListener("click", clickCutlery);
-    document.querySelector("#fork2").addEventListener("click", clickCutlery);
-    document.querySelector("#fork3").addEventListener("click", clickCutlery);
+    document.querySelector("#meatball").addEventListener("click", foodClick);
+    document.querySelector("#meatball2").addEventListener("click", foodClick);
+    document.querySelector("#meatball3").addEventListener("click", foodClick);
+    document.querySelector("#meatball4").addEventListener("click", foodClick);
+    document.querySelector("#meatball5").addEventListener("click", foodClick);
+    document.querySelector("#meatball6").addEventListener("click", foodClick);
+    document.querySelector("#pasta").addEventListener("click", foodClick);
+    document.querySelector("#pasta2").addEventListener("click", foodClick);
+    document.querySelector("#pasta3").addEventListener("click", foodClick);
+    document.querySelector("#pasta4").addEventListener("click", foodClick);
+    document.querySelector("#pasta5").addEventListener("click", foodClick);
+    document.querySelector("#knife").addEventListener("click", foodClick);
+    document.querySelector("#knife2").addEventListener("click", foodClick);
+    document.querySelector("#knife3").addEventListener("click", foodClick);
+    document.querySelector("#knife4").addEventListener("click", foodClick);
+    document.querySelector("#fork").addEventListener("click", foodClick);
+    document.querySelector("#fork2").addEventListener("click", foodClick);
+    document.querySelector("#fork3").addEventListener("click", foodClick);
     timeLeftFc();
 }
 
+function foodClick() {
+    console.log("foodClick");
 
-function clickFood() {
-    console.log("pasta klikket");
-
-    points++;
-    console.log(points);
-    document.querySelector("#points").innerHTML = points;
-    console.log(this);
     this.classList.add("hide");
-    this.removeEventListener("click", clickFood)
 
-    //
-    //        this.addEventListener("animationend", nySvamp);
-    //        gameStatus();
-}
 
-function clickCutlery() {
-    console.log("bestik klikket");
+    if (this.classList.contains("food")) {
+        document.querySelector("#sfx1").play();
+        document.querySelector("#sfx1").currentTime = 0;
+        console.log("pasta klikket");
 
-    life--;
-    console.log("life");
-    document.querySelector("#heart" + (life + 1)).classList.add("hide");
+        points++;
+        console.log(points);
+        document.querySelector("#points").innerHTML = points;
+        console.log(this);
+        this.classList.add("hide");
+        this.removeEventListener("click", foodClick)
+    } else if (this.classList.contains("cutlery")) {
+        document.querySelector("#sfx2").play();
+        document.querySelector("#sfx2").currentTime = 0;
+        console.log("bestik klikket");
 
-    this.addEventListener("animationend", timeLeftFc);
+        life--;
+        console.log("life");
+        document.querySelector("#heart" + (life + 1)).classList.add("hide");
+
+        this.addEventListener("animationend", timeLeftFc);
+
+    }
     gameStatus();
 }
+////function clickFood() {
+//console.log("pasta klikket");
+//
+//points++;
+//console.log(points);
+//document.querySelector("#points").innerHTML = points;
+//console.log(this);
+//this.classList.add("hide");
+//this.removeEventListener("click", clickFood)
+//
+//    //
+//    //        this.addEventListener("animationend", nySvamp);
+//    //        gameStatus();
+//}
+//
+//function clickCutlery() {
+//    console.log("bestik klikket");
+//
+//    life--;
+//    console.log("life");
+//    document.querySelector("#heart" + (life + 1)).classList.add("hide");
+//
+//    this.addEventListener("animationend", timeLeftFc);
+//    gameStatus();
+//}
 
 function timeLeftFc() {
     console.log("timeleftFc timeleft er" + timeLeft);
@@ -219,7 +207,7 @@ function gameStatus() {
     console.log(life);
     if (life == 0) {
         document.querySelector("#gameover").classList.remove("hide");
-    } else if (points == 10) {
+    } else if (points == 5) {
         document.querySelector("#levelcomplete").classList.remove("hide");
     }
 }
